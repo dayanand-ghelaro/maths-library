18d069863b1167fe4e1462ddadfa5f35e5804817
dayanand.ghelaro <meghwardayanand@gmail.com>
2023-10-27 18:43:29 +0500

    FEAT: Add Function for Multiplication

--- operations.py
+++ operations.py
@@ -1,1 +1,9 @@
 #It will contain math operations
+
+def multiply(num1, num2):
+    """Multiply two numbers."""
+    result = 0
+    for _ in range(num1):
+        result += num2
+    
+    return result
