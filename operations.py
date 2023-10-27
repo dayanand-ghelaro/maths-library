065a08266c7728f1acbbcb7c1e46bf0db13a1b21
dayanand.ghelaro <meghwardayanand@gmail.com>
2023-10-27 19:06:44 +0500

    REFACT: Refactor Multiplication Function

--- operations.py
+++ operations.py
@@ -3,11 +3,7 @@
 
 def multiply(num1, num2):
     """Multiply two numbers."""
-    result = 0
-    for _ in range(num1):
-        result += num2
-    
-    return result
+    return num1 * num2
 
 
 def add(num1, num2):
